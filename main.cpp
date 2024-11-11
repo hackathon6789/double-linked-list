6e4e569cb00725d76f6a260b232d90ceccbd122f
1621-roz <rozanega15@gmail.com>
2024-11-11 01:41:11 -0800

    update main program
    adding a class called DoublyLinkedList to the main file

--- main.cpp
+++ main.cpp
@@ -7,3 +7,11 @@ struct Node {
     Node* prev;
     Node(int val) : data(val), next(nullptr), prev(nullptr) {}
 };
+class DoublyLinkedList {
+private:
+    Node* head;
+    Node* tail;
+
+public:
+    DoublyLinkedList() : head(nullptr), tail(nullptr) {}
+}
