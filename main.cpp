fb987efbb4733c98ad36cc2d939e5d0ad0d688c2
Nunumare-lab <nunumare8@gmail.com>
2024-11-11 16:30:38 +0300

    update main.cpp
    insertion

--- main.cpp
+++ main.cpp
@@ -62,3 +62,51 @@ public:
         }
     }
 }
+ // Insert at the beginning
+    void insertAtBeginning(int data) {
+        Node* newNode = new Node(data);
+        if (!head) {
+            head = tail = newNode;
+        } else {
+            newNode->next = head;
+            head->prev = newNode;
+            head = newNode;
+        }
+    }
+
+    // Insert at the end
+    void insertAtEnd(int data) {
+        Node* newNode = new Node(data);
+        if (!tail) {
+            head = tail = newNode;
+        } else {
+            tail->next = newNode;
+            newNode->prev = tail;
+            tail = newNode;
+        }
+    }
+
+    // Insert at a specific position
+    void insertAtPosition(int data, int pos) {
+        if (pos <= 1) {
+            insertAtBeginning(data);
+            return;
+        }
+        Node* newNode = new Node(data);
+        Node* temp = head;
+        int count = 1;
+        while (temp && count < pos - 1) {
+            temp = temp->next;
+            count++;
+        }
+        if (!temp) {  // If position is out of bounds, insert at the end
+            insertAtEnd(data);
+        } else {
+            newNode->next = temp->next;
+            newNode->prev = temp;
+            if (temp->next) temp->next->prev = newNode;
+            temp->next = newNode;
+            if (!newNode->next) tail = newNode;
+        }
+    }
+
